ce601f0710f70d6d0d3bb31fc642ba77a824e01e
JeanDev2 <jeanpooldev@gmail.com>
2024-07-05 11:19:39 -0500

    chore: create base project files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Filters from "./components/Filters";
+import Layout from "./components/Layout";
+import Profile from "./components/Profile";
+import RepoList from "./components/Repo-list";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <Layout>
+      <Profile />
+      <Filters />
+      <RepoList />
+      <Search />
+    </Layout>
+  );
+}
+
+export default App;
